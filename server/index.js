cf347d8be84895ec11b529c639829e292c73c137
dezkowalski <dezkowalski@gmail.com>
2023-07-12 10:11:51 -0600

    add express

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
+app.use("./graphql", graphqlHTTP({}));
 app.listen(port, console.log(`server running on port ${port}`));
