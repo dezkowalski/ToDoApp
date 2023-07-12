819cfc40089c47e5fd968941eb36b51e6ad90219
dezkowalski <dezkowalski@gmail.com>
2023-07-12 10:07:39 -0600

    more stuff

---
+++ server/index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+app.listen(port, console.log(`server running on port ${port}`));
