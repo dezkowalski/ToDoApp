775e8a640643c712f432e1a014d018b1a6a82f1f
dezkowalski <dezkowalski@gmail.com>
2023-07-12 10:23:07 -0600

    add sample data and schema

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("schema");
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
-app.use("./graphql", graphqlHTTP({}));
+app.use("./graphql", graphqlHTTP({ schema, graphiql: process.env.NODE_ENV === "development" }));
 app.listen(port, console.log(`server running on port ${port}`));
---
+++ server/sampleData.js
@@ -0,0 +1,8 @@
+const todos = [
+  { id: '1', title: 'trash boi', 'completed': true },
+  { id: '2', title: 'dishes boi', 'completed': false },
+  { id: '3', title: 'laundry boi', 'completed': true },
+  { id: '1', title: 'pay bills boi', 'completed': false }
+];
+
+module.exports = {todos};
