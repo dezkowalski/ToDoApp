612cd648376c182bae8bb7b90d0fbf12b5cc48b8
dezkowalski <dezkowalski@gmail.com>
2023-07-26 09:58:32 -0600

    a couple fixes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
-const schema = require("schema");
+const schema = require("./schema");
 
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
-app.use("./graphql", graphqlHTTP({ schema, graphiql: process.env.NODE_ENV === "development" }));
+app.use("/graphql", graphqlHTTP({ schema, graphiql: process.env.NODE_ENV === "development" }));
 app.listen(port, console.log(`server running on port ${port}`));
--- server/sampleData.js
+++ server/sampleData.js
@@ -2,7 +2,7 @@ const todos = [
   { id: '1', title: 'trash boi', 'completed': true },
   { id: '2', title: 'dishes boi', 'completed': false },
   { id: '3', title: 'laundry boi', 'completed': true },
-  { id: '1', title: 'pay bills boi', 'completed': false }
+  { id: '4', title: 'pay bills boi', 'completed': false }
 ];
 
 module.exports = {todos};
