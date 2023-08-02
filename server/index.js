5abc049aa23ea0f7865010d34702bbd41cd3b020
dezkowalski <dezkowalski@gmail.com>
2023-08-02 09:57:42 -0600

    add mongo db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema");
-
+const connectDB = require("./db");
 const express = require('express');
+
+connectDB();
 const app = express();
 const port = process.env.PORT || 8000;
 app.use("/graphql", graphqlHTTP({ schema, graphiql: process.env.NODE_ENV === "development" }));
