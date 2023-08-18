8fc340e6f7e3fd8dab17911ee135edd62fb2c687
dezkowalski <dezkowalski@gmail.com>
2023-08-18 10:29:21 -0600

    add bootstrap stuffs and client to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+  cache: new InMemoryCache(),
+})
+
 function App() {
   return (
-    <div className="App">
-      <h1>Todo App</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Todo App</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
